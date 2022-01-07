92c49096c2ab8959bb9bac22625444c2035bee27
Joy <joyghosh381@gmailcom>
2022-01-08 00:21:11 +0530

    function added

--- src/components/Clac.js
+++ src/components/Clac.js
@@ -1,41 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
 const Clac = () => {
+  const [result, setresult] = useState("");
+
+  const handleClick = (e) => {
+    setresult(result.concat(e.target.name));
+  };
+
+  const clear = () => {
+    setresult("");
+  };
+  const backspace = () => {
+    setresult(result.slice(0, result.length - 1));
+  };
+
+  const claculate = () => {
+    try {
+      setresult(eval(result).toString());
+    } catch (err) {
+      setresult("Error");
+    }
+  };
   return (
     <>
-      <div className="answer">1050</div>
+      <div className="answer">
+        <form action="">
+          <input type="text" value={result} />
+        </form>
+      </div>
       <div className="all_btn">
         <div className="btn_rows">
           <div className="row">
-            <button className="btns c">C</button>
-            <button className="btns sign +/-">+/-</button>
-            <button className="btns sign %">%</button>
-            <button className="btns sign ÷">÷</button>
+            <button onClick={clear} id="clear" className="btns clear">
+              Clear
+            </button>
+            <button onClick={backspace} id="backspace" className="btns c">
+              C
+            </button>
+            <button name="%" onClick={handleClick} className="btns sign %">
+              %
+            </button>
+            <button name="/" onClick={handleClick} className="btns sign ÷">
+              ÷
+            </button>
           </div>
           <div className="row">
-            <button className="btns 7">7</button>
-            <button className="btns 8">8</button>
-            <button className="btns 9">9</button>
-            <button className="btns sign X">X</button>
+            <button name="7" onClick={handleClick} className="btns 7">
+              7
+            </button>
+            <button name="8" onClick={handleClick} className="btns 8">
+              8
+            </button>
+            <button name="9" onClick={handleClick} className="btns 9">
+              9
+            </button>
+            <button name="*" onClick={handleClick} className="btns sign ×">
+              ×
+            </button>
           </div>
           <div className="row">
-            <button className="btns 4">4</button>
-            <button className="btns 5">5</button>
-            <button className="btns 6">6</button>
-            <button className="btns sign -">-</button>
+            <button name="4" onClick={handleClick} className="btns 4">
+              4
+            </button>
+            <button name="5" onClick={handleClick} className="btns 5">
+              5
+            </button>
+            <button name="6" onClick={handleClick} className="btns 6">
+              6
+            </button>
+            <button name="-" onClick={handleClick} className="btns sign -">
+              -
+            </button>
           </div>
           <div className="row">
-            <button className="btns 1">1</button>
-            <button className="btns 2">2</button>
-            <button className="btns 3">3</button>
-            <button className="btns sign +">+</button>
+            <button name="1" onClick={handleClick} className="btns 1">
+              1
+            </button>
+            <button name="2" onClick={handleClick} className="btns 2">
+              2
+            </button>
+            <button name="3" onClick={handleClick} className="btns 3">
+              3
+            </button>
+            <button name="+" onClick={handleClick} className="btns sign +">
+              +
+            </button>
           </div>
           <div className="row last_row">
-            {/* <button className="btns">1</button> */}
-            <button className="btns 0">0</button>
-            <button className="btns dot">.</button>
-            <button className="btns equal">=</button>
+            <button onClick={handleClick} className="hide"></button>
+            <button name="0" onClick={handleClick} className="btns 0">
+              0
+            </button>
+            <button name="." onClick={handleClick} className="btns dot">
+              .
+            </button>
+            <button onClick={claculate} id="result" className="btns equal">
+              =
+            </button>
           </div>
         </div>
       </div>
